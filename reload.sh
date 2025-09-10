adf68b41610ed9b9acf1a62492838f1531c2c019
Richard T. Carback III <rick.carback@gmail.com>
2025-09-10 10:46:00 -0400

    Add reinstall for public genesis block

--- reload.sh
+++ reload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo cp genesis_block_public.json /etc/quip.network/genesis_block.json
 sudo cp -r . /opt/quip/src
 sudo chown -R quip:quip /opt/quip/src
 sudo -u quip /opt/quip/bin/pip install -e /opt/quip/src
