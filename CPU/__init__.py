80cc94c60eab3a7702e7e0508385a6db5c4c3c9a
Richard T. Carback III <rick.carback@gmail.com>
2026-06-03 17:21:20 -0400

    refactor(gpu,qpu,cpu): convert __init__ relative imports to absolute
    Simplification audit: replace from .x import with from PKG.x import, drop commented-out worker tombstones, and deduplicate GPU __all__ Modal entries.

--- CPU/__init__.py
+++ CPU/__init__.py
@@ -1,7 +1,6 @@
 """CPU mining components for quantum blockchain."""
 
-from .sa_sampler import SimulatedAnnealingStructuredSampler
-# from .worker import cpu_mine_block_process  # Not used
-from .sa_miner import SimulatedAnnealingMiner
+from CPU.sa_sampler import SimulatedAnnealingStructuredSampler
+from CPU.sa_miner import SimulatedAnnealingMiner
 
 __all__ = ['SimulatedAnnealingStructuredSampler', 'SimulatedAnnealingMiner']
