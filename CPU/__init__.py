88d68145f117411882e56afbbf4e0dc4bdf0fb26
Richard T. Carback III <rick.carback@gmail.com>
2026-07-25 01:11:19 +0000

    chore(cutover): delete v0.2 Python monolith; miners+chain are Rust now

--- CPU/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CPU mining components for quantum blockchain."""
-
-from CPU.sa_sampler import SimulatedAnnealingStructuredSampler
-from CPU.sa_miner import SimulatedAnnealingMiner
-
-__all__ = ['SimulatedAnnealingStructuredSampler', 'SimulatedAnnealingMiner']
