57c7860e49baac783b69003a27e4f61e6e347c80
Richard T. Carback III <rick.carback@gmail.com>
2026-07-25 00:18:14 +0000

    chore(cutover): remove extracted miner crates from workspace

--- python/quip_miner_dwave/__main__.py
+++
@@ -1,9 +0,0 @@
-"""``python -m quip_miner_dwave`` entry point."""
-from __future__ import annotations
-
-import sys
-
-from quip_miner_dwave.cli import main
-
-if __name__ == "__main__":
-    sys.exit(main())
